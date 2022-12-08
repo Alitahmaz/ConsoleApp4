9c187290d71ec8c515d720f63f2f505fd50eab56
Acsescity <Acsescity@DESKTOP-NONREC2>
2022-12-08 19:03:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            int a = 6;
+            int b = 6;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
